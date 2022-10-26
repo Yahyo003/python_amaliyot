695a9fb3c3a3891975a10b1b5712a5a5f38b4f79
Black Wolf <94526184+Yahyo003@users.noreply.github.com>
2022-10-26 09:21:49 +0500

    Add files via upload

---
+++ 3-amaliy-ish.py
@@ -0,0 +1,57 @@
+a=int(input("a >>> "))
+b=int(input("b >>> "))
+if a>0 and b>0:
+    print("sonlarning ikkisi ham juft")
+elif (a>0 and b<0) or (a<0 and b>0) or (a<0 and b<0):
+    print("bittasi toq yoki ikkitasi ham toq son")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
