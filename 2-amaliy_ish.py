010f2cd25bc038b630187b71aad97438533b0f10
Black Wolf <94526184+Yahyo003@users.noreply.github.com>
2022-10-26 09:08:08 +0500

    Add files via upload

---
+++ 2-amaliy_ish.py
@@ -0,0 +1,23 @@
+a=int(input("a >>> "))
+b=int(input("b >>> "))
+h=int(input("h balandlik >>> "))
+s=((a+b) / 2) * h
+
+print(f"berilgan {a} va {b} asoslari va {h} balanlik \n yuzasi = {s} ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
