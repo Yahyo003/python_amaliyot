5dca11cea90770e1817e30d62c2313685e0529cf
Black Wolf <94526184+Yahyo003@users.noreply.github.com>
2022-10-26 09:05:41 +0500

    Add files via upload

---
+++ 1-amaliy_ish.py
@@ -0,0 +1,8 @@
+
+# kiritilgan sm dagi o'lchamni m ga o'tkazib beruvchi dastur
+
+a = int(input("Necha sm kiritmoqchisiz >>> "))    # foydalanuvchidan sm da qabul qilinyapti
+s = a / 100      # olingan "sm" ni "m" ga otkazish uchun 100 ga bo'lishimiz kerak 
+# dasturimiz tayyor bo'ldi endi uni foydalanuvchiga ko'rsatamiz
+print(f"Siz kiritgan {a} sm bizning hisob kitobimizga ko'ra {s}\
+     m ga teng ekan \n{a} sm = {s} m teng")
